7a81324ac9f838ff56cf801458693843bc60030a
Jake Jepson <jwjepson1@gmail.com>
2022-09-04 15:36:31 -0400

    create module for game & store board array inside

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-const board = [];
-
 const squares = document.querySelectorAll(".square");
 
 squares.forEach((square) => {
@@ -8,7 +6,7 @@ squares.forEach((square) => {
 
 function addMarker() {
     this.textContent = player1.marker; // currentPlayer.marker
-    board[this.dataset.index] = this.textContent;
+    Game.board[this.dataset.index] = this.textContent;
 }
 
 const Player = (name, marker) => {
@@ -17,3 +15,8 @@ const Player = (name, marker) => {
 
 const player1 = Player("Jake", "X");
 const player2 = Player("Computer", "O");
+
+const Game = (() => {
+    const board = [];
+    return { board };
+})();
