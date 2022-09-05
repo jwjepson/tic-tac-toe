826d71a35de186858b14cd06bf5cba572b8cc0b4
Jake Jepson <jwjepson1@gmail.com>
2022-09-04 21:27:23 -0400

    create winningCombination list & check for winner

--- scripts.js
+++ scripts.js
@@ -5,6 +5,16 @@ const Player = (name, marker) => {
 const GameBoard = (() => {
     const player1 = Player("Jake", "X");
     const player2 = Player("Computer", "O");
+    const winningCombinations = [
+        [0,3,6],
+        [0,4,8],
+        [0,1,2],
+        [3,4,5],
+        [2,4,6],
+        [2,5,8],
+        [1,4,7],
+        [6,7,8],
+    ]
     const board = [];
     let currentPlayer = player1;
     const switchPlayer = () => {
@@ -15,14 +25,24 @@ const GameBoard = (() => {
             currentPlayer = player1;
         }
     }
+    const checkForWinner = () => {
+        for (let i = 0; i < winningCombinations.length; i++) {
+            if (currentPlayer.marker == board[winningCombinations[i][0]] && currentPlayer.marker == board[winningCombinations[i][1]] && currentPlayer.marker == board[winningCombinations[i][2]]) {
+                return true;
+            }
+        }
+        return false;
+    }
     const addMarker = () => {
         const squares = document.querySelectorAll(".square");
         squares.forEach((square) => {
             square.addEventListener("click", () => {
                 if (isValid(square)) {
-                    console.log(square);
                     board[square.dataset.index] = currentPlayer.marker // currentPlayer.marker
                     square.textContent = board[square.dataset.index];
+                    if (checkForWinner()) {
+                        alert(`${currentPlayer.name} is the winner!`);
+                    }
                     switchPlayer();
                 }
             });
